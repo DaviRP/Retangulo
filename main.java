90c887846358140fdce0025425e5e75ff8fdb4c9
DaviRP <davi.pegoraro@hotmail.com>
2015-04-27 11:26:36 -0300

    lol

---
+++ main.java
@@ -0,0 +1,13 @@
+class main{
+	public static void main(String args[]){
+	retangulo r1;
+	r1=new retangulo();
+
+	r1.width=88.9;
+	r1.height=78.3;
+
+    System.out.println(r1.area());
+
+    System.out.println(r1.perimetro());
+    }
+}
---
+++ retangulo.java
@@ -0,0 +1,21 @@
+class retangulo{
+	public Double alt;
+	public Double larg;
+
+
+	public void perimetro(){
+
+		int total;
+		total=(2*this.alt+2*this.larg);
+
+		return total,
+	}
+
+	public void area(){
+
+		int total;
+		total=this.alt*this.larg;
+		return total;
+	}
+	
+}
